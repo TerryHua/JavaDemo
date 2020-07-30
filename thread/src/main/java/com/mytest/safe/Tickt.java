2d4f6b72c366d5e61fc6abdf8b37894ad2229ded
huaxiaofeng <huaxiaofeng2@163.com>
2020-07-30 22:48:36 +0800

    线程安全问题

---
+++ thread/src/main/java/com/mytest/safe/Tickt.java
@@ -0,0 +1,20 @@
+package com.mytest.safe;
+
+public class Tickt implements Runnable {
+
+    private int ticktNum = 100;
+    public void run() {
+
+        while(true) {
+            if (ticktNum >0) {
+                try {
+                    Thread.sleep(100L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                String name = Thread.currentThread().getName();
+                System.out.println("线程" + name + "售票" + ticktNum--);
+            }
+        }
+    }
+}
